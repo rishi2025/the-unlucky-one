ef7bcda77c75bed9b85c40922744bdacd960333b
rishi2025 <99127485+rishi2025@users.noreply.github.com>
2023-07-25 12:04:25 +0530

    Added features
    Added new gaem functionality
    Winner celebration

--- script.js
+++ script.js
@@ -8,16 +8,36 @@ const cs1el = document.querySelector('.currScore1');
 const cs2el = document.querySelector('.currScore2');
 const score = [0, 0];
 let currPlayer = 0, currScore = 0;
-document.querySelector('.player1').classList.add('activePlayer');
-document.querySelector('.player2').classList.add('inactivePlayer');
-document.querySelector('.currBox1').classList.add('activeCurrBox');
-document.querySelector('.currBox2').classList.add('inactiveCurrBox');
+let playing = true;
 
-score1.textContent = 0;
-score2.textContent = 0;
+const init = function () {
+    currPlayer = 0, currScore = 0;
+    playing = true;
+    score1.textContent = 0;
+    score2.textContent = 0;
+    // Hiding dice
+    dice.classList.add('hidden');
+    score[0] = 0;
+    score[1] = 0;
+    const temp = function (cp) {
+        document.querySelector(`.player${cp + 1}`).classList.remove('activePlayer');
+        document.querySelector(`.player${cp + 1}`).classList.remove('winnerPlayer');
+        document.querySelector(`.currScore${cp + 1}`).textContent = "0";
+        document.querySelector(`.currText${cp + 1}`).textContent = "CURRENT";
+        document.querySelector(`.currScore${(cp ^ 1) + 1}`).textContent = "0";
+        document.querySelector(`.currText${(cp ^ 1) + 1}`).textContent = "CURRENT";
+        document.querySelector(`.player${(cp ^ 1) + 1}`).classList.remove('inactivePlayer');
+        document.querySelector(`.player${(cp ^ 1) + 1}`).classList.remove('loserPlayer');
+    }
+    temp(1);
+    temp(0);
+    document.querySelector('.player1').classList.add('activePlayer');
+    document.querySelector('.player2').classList.add('inactivePlayer');
+    document.querySelector('.currBox1').classList.add('activeCurrBox');
+    document.querySelector('.currBox2').classList.add('inactiveCurrBox');
+};
 
-// Hiding dice
-dice.classList.add('hidden');
+init();
 
 const swt = function (cp) {
     document.querySelector(`.player${cp + 1}`).classList.add('activePlayer');
@@ -32,43 +52,58 @@ const swt = function (cp) {
 
 roll.addEventListener('click', function () {
     // Rolling dice
-    const d = Math.trunc(Math.random() * 6) + 1;
+    if (playing) {
+        const d = Math.trunc(Math.random() * 6) + 1;
 
-    // Dice picture
-    dice.classList.remove('hidden');
-    dice.src = `d${d}.png`;
-    console.log(`d${d}.png`);
+        // Dice picture
+        dice.classList.remove('hidden');
+        dice.src = `d${d}.png`;
+        console.log(`d${d}.png`);
 
-    // Active Player
-    if (d !== 1)
-    {
-        currScore += d;
-        document.querySelector(`.currScore${currPlayer + 1}`).textContent = currScore;
-    }
-    else
-    {
-        document.querySelector(`.currScore${currPlayer + 1}`).textContent = 0;
-        currPlayer ^= 1;
-        currScore = 0;
-        swt(currPlayer);
+        // Active Player
+        if (d !== 1) {
+            currScore += d;
+            document.querySelector(`.currScore${currPlayer + 1}`).textContent = currScore;
+        }
+        else {
+            document.querySelector(`.currScore${currPlayer + 1}`).textContent = 0;
+            currPlayer ^= 1;
+            currScore = 0;
+            swt(currPlayer);
+        }
     }
 });
 
 document.querySelector('.hold').addEventListener('click', function () {
     // Adding current score to active player
-    score[currPlayer] += currScore;
-    score1.textContent = score[0];
-    score2.textContent = score[1];
+    if (playing) {
+        score[currPlayer] += currScore;
+        score1.textContent = score[0];
+        score2.textContent = score[1];
 
-    // If user reaches 100
-    //
-    //
-    //
-    //
+        // If user reaches 100
+        if (score[currPlayer] >= 10) {
+            playing = false;
+            document.querySelector(`.player${currPlayer + 1}`).classList.remove('activePlayer');
+            document.querySelector(`.player${currPlayer + 1}`).classList.add('winnerPlayer');
+            document.querySelector(`.currScore${currPlayer + 1}`).textContent = "🎉";
+            document.querySelector(`.currText${currPlayer + 1}`).textContent = "WIN";
+            document.querySelector(`.currScore${(currPlayer ^ 1) + 1}`).textContent = "☠️";
+            document.querySelector(`.currText${(currPlayer ^ 1) + 1}`).textContent = "LOSE";
+            document.querySelector(`.player${(currPlayer ^ 1) + 1}`).classList.remove('inactivePlayer');
+            document.querySelector(`.player${(currPlayer ^ 1) + 1}`).classList.add('loserPlayer');
+            dice.classList.add('hidden');
+        }
 
-    // Swatiching active player
-    document.querySelector(`.currScore${currPlayer + 1}`).textContent = 0;
-    currPlayer ^= 1;
-    currScore = 0;
-    swt(currPlayer);
+        // Switching active player
+        else {
+            document.querySelector(`.currScore${currPlayer + 1}`).textContent = 0;
+            currPlayer ^= 1;
+            currScore = 0;
+            swt(currPlayer);
+        }
+    }
 });
+
+//Resetting the game
+document.querySelector('.newGame').addEventListener('click', init);
